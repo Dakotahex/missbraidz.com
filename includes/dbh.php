441753160ae5dce43acd0feb4a4dc4056111c667
Dakota <dakota.colborne@gmail.com>
2022-12-27 16:19:09 +0800

    test

--- includes/dbh.php
+++ includes/dbh.php
@@ -4,6 +4,6 @@ $database = "u243262170_missbraidz";
 $username = "u243262170_dakota";
 $password = "Hercules14";
 
-$conn = mysqli_connect($servername, $username, $password, $databse);
+$conn = mysqli_connect($servername, $username, $password, $database);
 ?>
 
