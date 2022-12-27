9e565a36b7530c5156da97dbb39c4c2085c39672
Dakota <dakota.colborne@gmail.com>
2022-12-27 17:21:14 +0800

    test

--- includes/connect.php
+++ includes/connect.php
@@ -1,27 +1,26 @@
 <?php
-  include_once 'includes/dbh.php';
+  include_once 'dbh.php';
+  date_default_timezone_set('Australia/Perth');
 
   $firstName = $_POST['firstName'];
   $lastName = $_POST['lastName'];
   $email = $_POST['email'];
   $message = $_POST['message'];
+  $subscribe = $_POST['subscribe?'];
+  $date = date('Y-m-d h:i:s');
 
   //Database connection
   if($conn->connect_error){
     die('Connection Failed : '.$conn->connect_error);
   }
 
-  $sql = "INSERT INTO website_queries (firstName, lastName, email, message) VALUES ('$firstName', '$lastName', '$email', '$message')";
+  $sql = "INSERT INTO website_queries (firstName, lastName, email, message, date) VALUES ('$firstName', '$lastName', '$email', '$message', '$date')";
 
   if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    header("location: ../contact?message=successs");
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
 
   mysqli_close($conn);
-
-  // header("location: ../contact.html?message=successs")
-
-  // If subscribe was clicked also create user
 ?>
