509c6147671dde12789a4fd34b38e8b1f278b9a9
Dakota <dakota.colborne@gmail.com>
2022-12-28 09:16:22 +0800

    test

--- includes/connect.php
+++ includes/connect.php
@@ -7,14 +7,14 @@
   $email = $_POST['email'];
   $message = $_POST['message'];
   $subscribe = intVal($_POST['subscribe?']);
-  $date = date('Y-m-d H:i:s');
+  $createdAt = date('Y-m-d H:i:s');
 
   //Database connection
   if($conn->connect_error){
     die('Connection Failed : '.$conn->connect_error);
   }
 
-  $sql = "INSERT INTO website_queries (firstName, lastName, email, date, message, subscribed) VALUES ('$firstName', '$lastName', '$email', '$date', '$message', $subscribe)";
+  $sql = "INSERT INTO website_queries (firstName, lastName, email, createdAt, message, subscribed) VALUES ('$firstName', '$lastName', '$email', '$createdAt', '$message', $subscribe)";
 
   if (mysqli_query($conn, $sql)) {
     header("location: ../contact?message=successs");
