a04415caa4deae98df1408fec814754f1abf0130
Dakota <dakota.colborne@gmail.com>
2022-12-28 08:55:19 +0800

    test

--- includes/connect.php
+++ includes/connect.php
@@ -14,7 +14,7 @@
     die('Connection Failed : '.$conn->connect_error);
   }
 
-  $sql = "INSERT INTO website_queries (firstName, lastName, email, message, date, subscribed) VALUES ('$firstName', '$lastName', '$email', '$message', '$date', $subscribe)";
+  $sql = "INSERT INTO website_queries (firstName, lastName, email, message, date, subscribed) VALUES ('$firstName', '$lastName', '$email', '$message', $date, $subscribe)";
 
   if (mysqli_query($conn, $sql)) {
     header("location: ../contact?message=successs");
