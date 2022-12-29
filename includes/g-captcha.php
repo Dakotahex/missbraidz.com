d5f03a30a00644a8cc1b8ea0abe342b4fe14945b
Dakota <dakota.colborne@gmail.com>
2022-12-29 20:56:52 +0800

    adding google captcha v2

--- includes/g-captcha.php
+++ includes/g-captcha.php
@@ -1,4 +1,7 @@
 <?php
+;
+  $public_key = "6LcTL7MjAAAAAI-kqtMfpPXgzwqITD-0Ze414DM2";
+  $private_key = "6LcTL7MjAAAAACi8yteXhzSXKAHKGfIpDXeZq3Dx";
 
   if(array_key_exists('submit_form',$_POST))
   {
