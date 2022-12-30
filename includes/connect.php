c70cc1600e701c2cb392872a65e46dec0710b677
Dakota <dakota.colborne@gmail.com>
2022-12-30 09:50:50 +0800

    testing g-captcha form integration

--- includes/connect.php
+++ includes/connect.php
@@ -1,26 +1,35 @@
 <?php
   include_once 'dbh.php';
+  include 'g-captcha.php';
   date_default_timezone_set('UTC');
 
-  $firstName = $_POST['firstName'];
-  $lastName = $_POST['lastName'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $subscribe = intVal($_POST['subscribe?']);
-  $createdAt = date('Y-m-d H:i:s');
+  if (recaptchaTest() == TRUE)
+  {
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $subscribe = intVal($_POST['subscribe?']);
+    $createdAt = date('Y-m-d H:i:s');
 
-  //Database connection
-  if($conn->connect_error){
-    die('Connection Failed : '.$conn->connect_error);
+    //Database connection
+    if($conn->connect_error){
+      die('Connection Failed : '.$conn->connect_error);
+    }
+
+    $sql = "INSERT INTO website_queries (firstName, lastName, email, createdAt, message, subscribed) VALUES ('$firstName', '$lastName', '$email', '$createdAt', '$message', $subscribe)";
+
+    if (mysqli_query($conn, $sql)) {
+      header("location: ../contact?message=successs");
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
   }
+  else
+  {
+    echo "google captcha failed. Your message was not sent.";
+  };
 
-  $sql = "INSERT INTO website_queries (firstName, lastName, email, createdAt, message, subscribed) VALUES ('$firstName', '$lastName', '$email', '$createdAt', '$message', $subscribe)";
-
-  if (mysqli_query($conn, $sql)) {
-    header("location: ../contact?message=successs");
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-
-  mysqli_close($conn);
 ?>
--- includes/g-captcha.php
+++ includes/g-captcha.php
@@ -1,11 +1,31 @@
 <?php
-;
+
   $public_key = "6LcTL7MjAAAAAI-kqtMfpPXgzwqITD-0Ze414DM2";
   $private_key = "6LcTL7MjAAAAACi8yteXhzSXKAHKGfIpDXeZq3Dx";
+  $url = "https://www.google.com/recaptcha/api/siteverify";
 
-  if(array_key_exists('submit_form',$_POST))
-  {
-    echo "<pre>";print_r($_POST);echo "</pre>";
-  }
+
+  function recaptchaTest()  {
+    if(array_key_exists('submit_form',$_POST))
+    {
+      // echo "<pre>";print_r($_POST);echo "</pre>";
+      $response_key = $_POST['g-recaptcha-response'];
+      $response = file_get_contents($url.'?secret='.$private_key.'&response='.$response_key.'&remoteip='.$_SERVER['REMOTE_ADDR']);
+      $response = json_decode($response);
+
+      // echo "<pre>";print_r($response);echo "</pre>";
+
+      if($response->success = 1)
+      {
+        // echo "Your Information was valid";
+        return TRUE;
+      }
+      else
+      {
+        // echo "You are a robot and we don't like robots.";
+        return FALSE;
+      };
+    };
+  };
 
 ?>
