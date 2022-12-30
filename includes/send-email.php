fb8d73a85b79ea192225fa94c46a231914e369bf
Dakota <dakota.colborne@gmail.com>
2022-12-30 11:24:26 +0800

    test

--- includes/send-email.php
+++ includes/send-email.php
@@ -1,17 +1,41 @@
 <?php
 
+// function IsInjected($str)
+// {
+//   $injections = array('(\n+)',
+//   '(\r+)',
+//   '(\t+)',
+//   '(%0A+)',
+//   '(%0D+)',
+//   '(%08+)',
+//   '(%09+)'
+//            );
+
+//            $inject = join('|', $injections);
+//            $inject = "/$inject/i";
+
+//            if(preg_match($inject,$str))
+//            {
+//              return true;
+//             }
+//     else
+//     {
+//       return false;
+//     }
+//   }
+
   function sendEmail($firstName, $lastName, $email, $message)
   {
-    if(IsInjected($email))
-    {
-      echo "Bad email value!";
-      exit;
-    }
+    // if(IsInjected($email))
+    // {
+    //   echo "Bad email value!";
+    //   exit;
+    // }
 
     $email_from = $email;
     $email_subject = "New enquiry from {$firstName} {$lastName}";
     $email_body = "You have received a new message from {$firstName} {$lastName}.\n".
-    "Here is the message:\n $message";
+    "Here is the message:\n {$message}";
 
 
     $to = "enquiries@missbraidz.com";
@@ -19,30 +43,7 @@
     $headers .= "Reply-To: $email \r\n";
 
     mail($to, $email_subject, $email_body, $headers);
+    echo "email sent!";
   }
 
-  function IsInjected($str)
-  {
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-
-    if(preg_match($inject,$str))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-  }
-
-?>
+  ?>
